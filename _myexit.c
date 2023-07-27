48947b877805186029c08f553a1c5a2b4aff1651
Adham <adham.3llam@gmail.com>
2023-07-26 18:11:21 -0700

    modify

--- _myexit.c
+++ _myexit.c
@@ -8,10 +8,23 @@
 void _myexit(shell_data *data)
 {
 	char ***argv = &data->args;
-	int status = data->status;
+	int status = data->status, i, is_string = 0;
 
-	if ((*argv)[1])
-		status = _atoi((*argv)[1]);
+	for (i = 0; data->args[1][i] != '\0'; ++i)
+	{
+		if (data->args[1][i] < '0' || data->args[1][i] > '9')
+		{
+			dprintf(STDERR_FILENO, "%s: 1: exit: Illegal number: %s\n", data->av[0], data->args[1]);
+			status = 2;
+			is_string = 1;
+			break;
+		}
+	}
+	if (!is_string)
+	{
+		if ((*argv)[1])
+			status = _atoi((*argv)[1]);
+	}
 	if (status < 0)
 	{
 		dprintf(STDERR_FILENO, "%s: 1: exit: Illegal number: %i\n", data->av[0], status);
