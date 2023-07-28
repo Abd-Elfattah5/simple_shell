47c566823e52cc29c1d933e1c9f6b8d5cf567430
Adham <adham.3llam@gmail.com>
2023-07-27 17:48:52 -0700

    modify betty style

--- _myexit.c
+++ _myexit.c
@@ -46,9 +46,9 @@ void _myexit(shell_data *data)
 */
 void _perror(char *s, shell_data *data, int status)
 {
-        if (s)
-                printf("%s", s);
-        free_shell_data(data);
-        exit(status);
+	if (s)
+		printf("%s", s);
+	free_shell_data(data);
+	exit(status);
 }
 
